f65ab918171e275160421d4667e0ad2a141cdf50
avion <Avionk.smith@gmail.com>
2022-08-31 10:29:23 -0400

    Added auto increimentation too applicationa

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 @app.get("/", tags = ["ROOT"])
@@ -9,24 +8,34 @@ async def root() -> dict:
     return{"Hello": "world"}
 
 shoes = []
+count = 1
+
+
+
+
 #get --> shoes list with brand name
-@app.get("/shoe/", tags = ["shoes"])
-async def get_shoe() -> dict:
-    return{"data": shoes}
+@app.get("/shoe/{id}", tags = ["shoes"])
+async def get_shoe(id:int, body: dict) -> dict:
+    for shoe in dict:
+        pass
+       
+
    
 # post --> create new brand
 @app.post("/shoe", tags = ["shoes"])
-async def add_shoe(shoe:dict) -> dict:
-        shoes.append(shoe)
-        return{"data": "A Brand has been added !"}
+async def add_shoe(shoe_dict:dict) -> dict:
+    global count 
+    shoe_dict["id"] = count
+    count += 1
+    shoes.append(shoe_dict)        
+    return{"data": "A Brand has been added !"}
         
-
 # put --> update brand
 @app.put("/shoe/{id}", tags = ["shoes"])
 async def update_shoe(id: int, body: dict) -> dict:
     for shoe in shoes:
         if int((shoe ["id"])) == id:
-            shoe["Brand"] = body["Brand"]
+            shoe["brand"] = body["brand"]
             return {
                 "data": f"shoe with id {id} has been entered. "
             
@@ -34,12 +43,16 @@ async def update_shoe(id: int, body: dict) -> dict:
     return{
         "data": f"shoe with this id number {id} was not found !"
     }
-
+#get total list
+@app.get("/shoe", tags = ["shoes"])
+async def get_shoe() -> dict:
+    return{"data":shoes} 
 
 #Delete --> deleteting data
 @app.delete("/shoe/{id}", tags = ["shoes"])
 async def delete_shoe(id: int) -> dict:
     for shoe in shoes:
+        
         shoes.remove(shoe)
         return {"data": f"shoe has been deleted" } or {"data": f"shoe with id {id} has been deleted. "}
        
